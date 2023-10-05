1e2ed3c77520cc93b1f8ee32727c7631030d414f
khrabanm <khrabanm@yandex.ru>
2023-10-05 11:15:15 +0300

    refactor: remove comment

--- utils/validator.js
+++ utils/validator.js
@@ -1,6 +1,5 @@
 const {celebrate, Joi} = require('celebrate');
 
-// eslint-disable-next-line no-useless-escape
 const regex = /^(http|https):\/\/(www\.)?[a-zA-Z0-9\-._~:\/?#[\]@!$&'()*+,;=]{2,256}\.[a-zA-Z0-9.\/?#-]{2,}$/;
 
 const validateUser = celebrate({
