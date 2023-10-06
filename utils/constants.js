bbc465ab8fa7f429b8c45a392f1c0c576e106ba3
khrabanm <khrabanm@yandex.ru>
2023-10-07 01:08:08 +0300

    refactor: add ERROR_CODE_UNIQUE

--- utils/constants.js
+++ utils/constants.js
@@ -1,9 +1,11 @@
 const DEV_SECRET = 'dev-secret';
 const STATUS_OK = 200;
 const CREATED = 201;
+const ERROR_CODE_UNIQUE = 11000;
 
 module.exports = {
   DEV_SECRET,
   STATUS_OK,
   CREATED,
+  ERROR_CODE_UNIQUE,
 };
